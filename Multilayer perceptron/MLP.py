9e72e41bac191e06bc89097fc8ffa2686af915cf
ZoltanHJetsmen <zoltan.jetsmen@usp.br>
2018-09-07 04:33:37 +0000

    MLP working with N hidden layers

--- Multilayer perceptron/MLP.py
+++ Multilayer perceptron/MLP.py
@@ -15,7 +15,6 @@ def readDataset(filename):
     X = data.iloc[:,0:len(data.columns)-1]
     X = np.array(X)
 
-    # Shuffle Data
     indices = np.random.choice(len(X), len(X), replace=False)
     X_values = X[indices]
     y_values = y[indices]
@@ -26,9 +25,8 @@ def readDataset(filename):
 
     return d
 
-def processing(dataset):
+def processing(dataset, percentage):
 
-    #x = preprocessing.normalize(dataset.X, norm='l2')
     scaler = StandardScaler()
     scaler.fit(dataset.X)
     x = scaler.transform(dataset.X)
@@ -40,11 +38,11 @@ def processing(dataset):
     
     lenght = dataset.X.shape[0]
 
-    x_train = x[0:int(0.7*lenght), :]
-    y_train = y[0:int(0.7*lenght), :]
+    x_train = x[0:int(percentage*lenght), :]
+    y_train = y[0:int(percentage*lenght), :]
 
-    x_test = x[int(0.7*lenght):, :]
-    y_test = y[0:int(0.7*lenght), :]
+    x_test = x[int(percentage*lenght):, :]
+    y_test = y[0:int(percentage*lenght), :]
         
     dataset = namedtuple('datset', 'X Y')
 
@@ -54,68 +52,101 @@ def processing(dataset):
 
     return train, test
 
-# Definição da função sigmoid
 def sigmoid(x):
 
     return 1/(1+np.exp(-x))
 
 def mlp_forward(x, hidden_weights, output_weights):
 
-    net = np.matmul(x,hidden_weights[:,0:len(x)].transpose()) + hidden_weights[:,-1]
-    f_net_h = sigmoid(net)
+    f_net_h = []
 
-    net = np.matmul(f_net_h,output_weights[:,0:len(f_net_h)].transpose()) + output_weights[:,-1]
+    for i in range(len(hidden_weights)):
+        if i == 0:
+            net = np.matmul(x,hidden_weights[i][:,0:len(x)].transpose()) + hidden_weights[i][:,-1]
+            f_net = sigmoid(net)
+        else:
+            net = np.matmul(f_net_h[i-1],hidden_weights[i][:,0:len(f_net_h[i-1])].transpose()) + hidden_weights[i][:,-1]
+            f_net = sigmoid(net)
+        
+        f_net_h.append(f_net) 
+
+    net = np.matmul(f_net_h[len(f_net_h)-1],output_weights[:,0:len(f_net_h[len(f_net_h)-1])].transpose()) + output_weights[:,-1]
     f_net_o = sigmoid(net)
 
     return f_net_o, f_net_h
 
-def mlp_backward(dataset, i, hidden_weights, output_weights, f_net_o, f_net_h, alpha, hidden_units, n_classes):
+def mlp_backward(dataset, j, hidden_weights, output_weights, f_net_o, f_net_h, alpha, hidden_units, n_classes):
 
-    x = dataset.X[i,:]
-    y = dataset.Y[i,:]
+    x = dataset.X[j,:]
+    y = dataset.Y[j,:]
 
     error = y - f_net_o
 
-    #delta da output layer
     delta_o = error*f_net_o*(1-f_net_o)
-
-    #delta da hidden layer
-    w_o = output_weights[: ,0:hidden_units]
     
-    delta_h = (f_net_h*(1-f_net_h))*(np.matmul(delta_o, w_o))
+    delta_h = []
 
-    x_aux = np.concatenate((x,np.ones(1)))[np.newaxis, :]
-    delta_h = delta_h[:, np.newaxis]
+    for i in range(len(hidden_units)-1, -1, -1):
 
+        if(i == len(hidden_units)-1):
+            w_o = output_weights[: ,0:hidden_units[i]]
+            delta = (f_net_h[i]*(1-f_net_h[i]))*(np.matmul(delta_o, w_o))
+        else:
+            w_o = hidden_weights[i+1][:,0:hidden_units[i]]
+            delta = (f_net_h[i]*(1-f_net_h[i]))*(np.matmul(delta, w_o))
+
+        delta_h.insert(0,delta)
+        
     delta_o = delta_o[:, np.newaxis]
-    f_net_aux = np.concatenate((f_net_h,np.ones(1)))[np.newaxis, :]
-    
-    #print(delta_o)
-    #print(np.concatenate((f_net_h,np.ones(1))))
-
+    f_net_aux = np.concatenate((f_net_h[len(hidden_units)-1],np.ones(1)))[np.newaxis, :]
     output_weights = output_weights - -2*alpha*np.matmul(delta_o, f_net_aux)
+    
+    for i in range(len(hidden_units)-1, -1, -1):
+        delta = delta_h[i][:, np.newaxis]
+        f_net_aux = np.concatenate((f_net_h[i],np.ones(1)))[np.newaxis, :]    
 
-    hidden_weights = hidden_weights - -2*alpha*np.matmul(delta_h, x_aux)    
+        if i == 0:
+            x_aux = np.concatenate((x,np.ones(1)))[np.newaxis, :]
+            hidden_weights[i] = hidden_weights[i] - -2*alpha*np.matmul(delta, x_aux)
+        else:
+            f_net_aux = np.concatenate((f_net_h[i-1],np.ones(1)))[np.newaxis, :]
+            hidden_weights[i] = hidden_weights[i] - -2*alpha*np.matmul(delta, f_net_aux)
 
     error = sum(error*error)
 
     return hidden_weights, output_weights, error 
 
-def MLP(dataset, hidden_units, n_classes, threshold, alpha):
+def MLP(dataset, hidden_layers ,hidden_units, n_classes, threshold, alpha):
 
-    train, test = processing(dataset)
+    train, test = processing(dataset, 0.7)
 
-    print(train.X)
+    if(len(hidden_units) != hidden_layers):
+        print("The parameters hidden_layers and hidden_units must have the same value.")
+        return
 
-    hidden_weights = np.zeros((hidden_units, dataset.X.shape[1] + 1))
-    output_weights = np.zeros((n_classes, hidden_units+1))
+    hidden_weights = []
 
-    for i in range(hidden_units):
-        for j in range(dataset.X.shape[1] + 1):
-            hidden_weights[i][j] = random.uniform(-1, 1)
+    for i in range(hidden_layers):
+        if(i == 0):
+            aux = np.zeros((hidden_units[i], dataset.X.shape[1] + 1))
+        else:
+            aux = np.zeros((hidden_units[i], hidden_units[i-1] + 1))
+
+        hidden_weights.append(aux)
+    
+    for i in range(hidden_layers):
+        for j in range(hidden_units[i]):
+            if(i == 0):
+                for k in range(dataset.X.shape[1] + 1):
+                    hidden_weights[i][j][k] = random.uniform(-1, 1)
+            else:
+                for k in range(hidden_units[i-1]+1):
+                    hidden_weights[i][j][k] = random.uniform(-1, 1)
+
+    output_weights = np.zeros((n_classes, hidden_units[len(hidden_units)-1]+1))
 
     for i in range(n_classes):
-        for j in range(hidden_units+1):
+        for j in range(hidden_units[hidden_layers-1]+1):
             output_weights[i][j] = random.uniform(-1, 1)
 
     sum_errors = 2* threshold
@@ -125,7 +156,7 @@ def MLP(dataset, hidden_units, n_classes, threshold, alpha):
         for i in range(train.X.shape[0]):
             # Forward
             f_net_o, f_net_h = mlp_forward(train.X[i, :], hidden_weights, output_weights)
-
+            
             # Backward hidden_weights, output_weights, error = 
             hidden_weights, output_weights, error = mlp_backward(train, i, hidden_weights, output_weights, f_net_o, f_net_h, alpha, hidden_units, n_classes)
 
@@ -135,11 +166,9 @@ def MLP(dataset, hidden_units, n_classes, threshold, alpha):
         if(epoch % 100 == 0):
             print(sum_errors)
 
-    print(dataset.X)
-
-    for i in range(4):
+    """for i in range(4):
         y, q = mlp_forward(dataset.X[i,:], hidden_weights, output_weights)
         print(dataset.X[i,:])
-        print(y)
+        print(y)"""
 
-MLP(readDataset("wine.csv"), 2, 3, 0.01, 0.1)
+MLP(readDataset("wine.csv"), 3, [5, 3, 6] , 3, 0.01, 0.1)
